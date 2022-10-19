5179949ea3f4a077ab68978c60044663b54c5e01
sebax <sebaxtian.tc@gmail.com>
2022-10-18 22:32:43 -0500

    Initial Commit

---
+++ ExameniOS/ExameniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExameniOSApp.swift
+//  ExameniOS
+//
+//  Created by Sebastian TC on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ExameniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
